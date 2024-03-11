01a383aa4a33d46d92c9986e819a39fb99b3c5d9
FrnndSoul <125233489+FrnndSoul@users.noreply.github.com>
2024-03-11 15:05:09 +0800

    services flowlayout calendar flowlayout

--- SalonUserApp/User Controls/Appointment Folder/AppointmentDate.Designer.cs
+++ SalonUserApp/User Controls/Appointment Folder/AppointmentDate.Designer.cs
@@ -28,19 +28,225 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ServiceSelection = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.guna2GroupBox1 = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.btn11 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn12 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn1 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn4 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn3 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn2 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn7 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn6 = new Guna.UI2.WinForms.Guna2Button();
+            this.btn5 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2GroupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // ServiceSelection
+            // 
+            this.ServiceSelection.BorderColor = System.Drawing.Color.Black;
+            this.ServiceSelection.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ServiceSelection.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ServiceSelection.Font = new System.Drawing.Font("Chinacat", 18F);
+            this.ServiceSelection.ForeColor = System.Drawing.Color.Black;
+            this.ServiceSelection.Location = new System.Drawing.Point(3, 3);
+            this.ServiceSelection.Name = "ServiceSelection";
+            this.ServiceSelection.Size = new System.Drawing.Size(521, 515);
+            this.ServiceSelection.TabIndex = 27;
+            this.ServiceSelection.Text = "Choose Date";
+            // 
+            // guna2GroupBox1
+            // 
+            this.guna2GroupBox1.BorderColor = System.Drawing.Color.Black;
+            this.guna2GroupBox1.Controls.Add(this.btn7);
+            this.guna2GroupBox1.Controls.Add(this.btn6);
+            this.guna2GroupBox1.Controls.Add(this.btn5);
+            this.guna2GroupBox1.Controls.Add(this.btn4);
+            this.guna2GroupBox1.Controls.Add(this.btn3);
+            this.guna2GroupBox1.Controls.Add(this.btn2);
+            this.guna2GroupBox1.Controls.Add(this.btn1);
+            this.guna2GroupBox1.Controls.Add(this.btn12);
+            this.guna2GroupBox1.Controls.Add(this.btn11);
+            this.guna2GroupBox1.CustomBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.guna2GroupBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.guna2GroupBox1.Font = new System.Drawing.Font("Chinacat", 18F);
+            this.guna2GroupBox1.ForeColor = System.Drawing.Color.Black;
+            this.guna2GroupBox1.Location = new System.Drawing.Point(3, 524);
+            this.guna2GroupBox1.Name = "guna2GroupBox1";
+            this.guna2GroupBox1.Size = new System.Drawing.Size(521, 193);
+            this.guna2GroupBox1.TabIndex = 28;
+            this.guna2GroupBox1.Text = "Choose Time";
+            // 
+            // btn11
+            // 
+            this.btn11.BackColor = System.Drawing.Color.Transparent;
+            this.btn11.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn11.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn11.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn11.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn11.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn11.Font = new System.Drawing.Font("Chinacat", 18F);
+            this.btn11.ForeColor = System.Drawing.Color.Black;
+            this.btn11.Location = new System.Drawing.Point(3, 43);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(167, 45);
+            this.btn11.TabIndex = 0;
+            this.btn11.Text = "11:00am";
+            // 
+            // btn12
+            // 
+            this.btn12.BackColor = System.Drawing.Color.Transparent;
+            this.btn12.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn12.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn12.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn12.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn12.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn12.Font = new System.Drawing.Font("Chinacat", 18F);
+            this.btn12.ForeColor = System.Drawing.Color.Black;
+            this.btn12.Location = new System.Drawing.Point(176, 43);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(167, 45);
+            this.btn12.TabIndex = 1;
+            this.btn12.Text = "12:00nn";
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.Transparent;
+            this.btn1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn1.Font = new System.Drawing.Font("Chinacat", 18F);
+            this.btn1.ForeColor = System.Drawing.Color.Black;
+            this.btn1.Location = new System.Drawing.Point(349, 43);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(167, 45);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "01:00pm";
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.Transparent;
+            this.btn4.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn4.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn4.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn4.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn4.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn4.Font = new System.Drawing.Font("Chinacat", 18F);
+            this.btn4.ForeColor = System.Drawing.Color.Black;
+            this.btn4.Location = new System.Drawing.Point(349, 94);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(167, 45);
+            this.btn4.TabIndex = 5;
+            this.btn4.Text = "04:00pm";
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.Transparent;
+            this.btn3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn3.Font = new System.Drawing.Font("Chinacat", 18F);
+            this.btn3.ForeColor = System.Drawing.Color.Black;
+            this.btn3.Location = new System.Drawing.Point(176, 94);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(167, 45);
+            this.btn3.TabIndex = 4;
+            this.btn3.Text = "03:00pm";
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.Transparent;
+            this.btn2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn2.Font = new System.Drawing.Font("Chinacat", 18F);
+            this.btn2.ForeColor = System.Drawing.Color.Black;
+            this.btn2.Location = new System.Drawing.Point(3, 94);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(167, 45);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "02:00pm";
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.Transparent;
+            this.btn7.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn7.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn7.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn7.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn7.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn7.Font = new System.Drawing.Font("Chinacat", 18F);
+            this.btn7.ForeColor = System.Drawing.Color.Black;
+            this.btn7.Location = new System.Drawing.Point(349, 145);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(167, 45);
+            this.btn7.TabIndex = 8;
+            this.btn7.Text = "07:00pm";
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.Transparent;
+            this.btn6.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn6.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn6.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn6.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn6.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn6.Font = new System.Drawing.Font("Chinacat", 18F);
+            this.btn6.ForeColor = System.Drawing.Color.Black;
+            this.btn6.Location = new System.Drawing.Point(176, 145);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(167, 45);
+            this.btn6.TabIndex = 7;
+            this.btn6.Text = "06:00pm";
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.Transparent;
+            this.btn5.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn5.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn5.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn5.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn5.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn5.Font = new System.Drawing.Font("Chinacat", 18F);
+            this.btn5.ForeColor = System.Drawing.Color.Black;
+            this.btn5.Location = new System.Drawing.Point(3, 145);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(167, 45);
+            this.btn5.TabIndex = 6;
+            this.btn5.Text = "05:00pm";
             // 
             // AppointmentDate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.Controls.Add(this.guna2GroupBox1);
+            this.Controls.Add(this.ServiceSelection);
             this.Name = "AppointmentDate";
             this.Size = new System.Drawing.Size(1080, 720);
+            this.guna2GroupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Guna.UI2.WinForms.Guna2GroupBox ServiceSelection;
+        private Guna.UI2.WinForms.Guna2GroupBox guna2GroupBox1;
+        private Guna.UI2.WinForms.Guna2Button btn7;
+        private Guna.UI2.WinForms.Guna2Button btn6;
+        private Guna.UI2.WinForms.Guna2Button btn5;
+        private Guna.UI2.WinForms.Guna2Button btn4;
+        private Guna.UI2.WinForms.Guna2Button btn3;
+        private Guna.UI2.WinForms.Guna2Button btn2;
+        private Guna.UI2.WinForms.Guna2Button btn1;
+        private Guna.UI2.WinForms.Guna2Button btn12;
+        private Guna.UI2.WinForms.Guna2Button btn11;
     }
 }
